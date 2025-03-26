06e07853f9d9ff7a4fa45b37e20c93b314e5b4da
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-03-26 11:11:19 -0400

    feat(utils): remove local `deepMerge` in favor of the util packaged with Payload

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,5 +1,4 @@
-import deepMerge from './deepMerge';
 import { generatePreviewPath } from './generatePreviewPath';
 import isObject from './isObject';
 
-export { deepMerge, generatePreviewPath, isObject };
+export { generatePreviewPath, isObject };
