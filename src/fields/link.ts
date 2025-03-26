b3dc5bf18ff32c969dd8cb63b3368c8076bd8257
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-03-26 09:48:49 -0400

    fix(fields): make link field name consistent with other fields
    change `link` to `linkField`

--- src/fields/link.ts
+++ src/fields/link.ts
@@ -1,75 +1,75 @@
-import { Field, GroupField } from "payload";
+import { Field, GroupField } from 'payload';
 
 export type LinkAppearances = 'default'; // TODO: add more appearances
 
-export const appearanceOptions: Record<LinkAppearances, { label: string, value: string }> = {
-    default: {
-        label: 'Default (Text Link)',
-        value: 'default'
-    }
+export const appearanceOptions: Record<LinkAppearances, { label: string; value: string }> = {
+  default: {
+    label: 'Default (Text Link)',
+    value: 'default',
+  },
 };
 
 type LinkType = (options?: {
-    appearances?: LinkAppearances[] | false,
-    disableLabel?: boolean,
-    overrides?: Partial<GroupField>
+  appearances?: LinkAppearances[] | false;
+  disableLabel?: boolean;
+  overrides?: Partial<GroupField>;
 }) => Field;
 
-export const link: LinkType = ({ appearances, disableLabel = false, overrides = {} } = {}) => {
-    const linkResult: GroupField = {
-        name: 'link',
-        type: 'group',
-        admin: {
-            hideGutter: true,
-        },
+export const linkField: LinkType = ({ appearances, disableLabel = false, overrides = {} } = {}) => {
+  const linkResult: GroupField = {
+    name: 'link',
+    type: 'group',
+    admin: {
+      hideGutter: true,
+    },
+    fields: [
+      {
+        type: 'row',
         fields: [
-            {
-                type: 'row',
-                fields: [
-                    {
-                        name: 'type',
-                        type: 'radio',
-                        options: [
-                            {
-                                label: 'Internal Link',
-                                value: 'reference'
-                            },
-                            {
-                                label: 'Custom URL',
-                                value: 'custom'
-                            }
-                        ],
-                        defaultValue: 'reference',
-                        admin: {
-                            layout: 'horizontal',
-                            width: '50%',
-                        },
-                    },
-                    {
-                        name: 'newTab',
-                        label: 'Open in New Tab',
-                        type: 'checkbox',
-                        admin: {
-                            style: {
-                                alignSelf: 'flex-end',
-                            },
-                            width: '50%',
-                        },
-                    }
-                ]
-            }
-        ]
-    }
-
-    const linkTypes: Field[] = [
-        {
-            name: 'reference',
-            label: 'Document to Link To'
-            type: 'relationship',
-            relationTo: ['pages'], // TODO: add other collections as needed
+          {
+            name: 'type',
+            type: 'radio',
+            options: [
+              {
+                label: 'Internal Link',
+                value: 'reference',
+              },
+              {
+                label: 'Custom URL',
+                value: 'custom',
+              },
+            ],
+            defaultValue: 'reference',
             admin: {
-                condition: (_, siblingData) => siblingData?.type === 'reference'
-            }
-        }
-    ]
-}
+              layout: 'horizontal',
+              width: '50%',
+            },
+          },
+          {
+            name: 'newTab',
+            label: 'Open in New Tab',
+            type: 'checkbox',
+            admin: {
+              style: {
+                alignSelf: 'flex-end',
+              },
+              width: '50%',
+            },
+          },
+        ],
+      },
+    ],
+  };
+
+  const linkTypes: Field[] = [
+    {
+      name: 'reference',
+      label: 'Document to Link To',
+      type: 'relationship',
+      relationTo: ['pages'], // TODO: add other collections as needed
+      admin: {
+        condition: (_, siblingData) => siblingData?.type === 'reference',
+      },
+    },
+  ];
+};
