53a2696c22292df25f02db3e76eba8ed6e97d410
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-03-26 11:11:45 -0400

    feat(fields): complete linkField

--- src/fields/link.ts
+++ src/fields/link.ts
@@ -1,4 +1,4 @@
-import { Field, GroupField } from 'payload';
+import { deepMerge, Field, GroupField } from 'payload';
 
 export type LinkAppearances = 'default'; // TODO: add more appearances
 
@@ -66,10 +66,68 @@ export const linkField: LinkType = ({ appearances, disableLabel = false, overrid
       name: 'reference',
       label: 'Document to Link To',
       type: 'relationship',
-      relationTo: ['pages'], // TODO: add other collections as needed
+      relationTo: ['pages'], // Add other collections here
+      required: true,
       admin: {
         condition: (_, siblingData) => siblingData?.type === 'reference',
       },
     },
+    {
+      name: 'url',
+      label: 'Custom URL',
+      type: 'text',
+      required: true,
+      admin: {
+        condition: (_, siblingData) => siblingData?.type === 'custom',
+      },
+    },
   ];
+
+  if (!disableLabel) {
+    linkTypes.map((linkType) => ({
+      ...linkType,
+      admin: {
+        ...linkType.admin,
+        width: '100%',
+      },
+    }));
+
+    linkResult.fields.push({
+      type: 'row',
+      fields: [
+        ...linkTypes,
+        {
+          name: 'label',
+          label: 'Label',
+          type: 'text',
+          required: true,
+          admin: {
+            width: '50%',
+          },
+        },
+      ],
+    });
+  } else {
+    linkResult.fields = [...linkResult.fields, ...linkTypes];
+  }
+
+  if (appearances !== false) {
+    let appearanceOptionsToUse = [appearanceOptions.default];
+
+    if (appearances) {
+      appearanceOptionsToUse = appearances.map((appearance) => appearanceOptions[appearance]);
+    }
+
+    linkResult.fields.push({
+      name: 'appearance',
+      type: 'select',
+      defaultValue: 'default',
+      options: appearanceOptionsToUse,
+      admin: {
+        description: 'Choose how the link will be displayed.',
+      },
+    });
+  }
+
+  return deepMerge(linkResult, overrides);
 };
