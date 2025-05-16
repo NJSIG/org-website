e328973d634663d53572d6463de3ffa840a4f213
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-05-16 15:05:02 -0400

    feat: configured email transport (#6)

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer';
 import { redirectsPlugin } from '@payloadcms/plugin-redirects';
 import { lexicalEditor } from '@payloadcms/richtext-lexical';
 import path from 'path';
@@ -32,6 +33,19 @@ export default buildConfig({
   },
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
+  }),
+  email: nodemailerAdapter({
+    defaultFromAddress: 'noreply@njsig.org',
+    defaultFromName: 'NJSIG',
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT) || 587,
+      secure: process.env.SMTP_SECURE === 'true',
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    },
   }),
   sharp,
   plugins: [
