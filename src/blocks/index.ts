e32cdc0d2caba0a4a73ad9f173928536a86b898f
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-06-17 10:51:57 -0400

    fix(blocks): missing export for responsive cols

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,6 +1,7 @@
 import { HeroSpinner } from './HeroSpinner/config';
 import { HiddenTitle } from './HiddenTitle/config';
+import { ResponsiveColumns } from './ResponsiveColumns/config';
 import { Section } from './Section/config';
 import { SectionTitle } from './SectionTitle/config';
 
-export { HeroSpinner, HiddenTitle, Section, SectionTitle };
+export { HeroSpinner, HiddenTitle, ResponsiveColumns, Section, SectionTitle };
