ed4849e7ade4d4b0f0a34361f85e7ff6655bcbcc
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-06-18 14:34:18 -0400

    feat(bocks): add section content block config

---
+++ src/blocks/Section/blocks/SectionContent/config.ts
@@ -0,0 +1,43 @@
+import {
+  BlockquoteFeature,
+  BoldFeature,
+  FixedToolbarFeature,
+  HeadingFeature,
+  InlineToolbarFeature,
+  ItalicFeature,
+  lexicalEditor,
+  LinkFeature,
+  OrderedListFeature,
+  ParagraphFeature,
+  UnorderedListFeature,
+} from '@payloadcms/richtext-lexical';
+import { Block } from 'payload';
+
+export const SectionContent: Block = {
+  slug: 'sectionContent',
+  interfaceName: 'SectionContentBlock',
+  fields: [
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+      admin: {
+        description: 'Formatting options are limited to maintain consistency across the site.',
+      },
+      editor: lexicalEditor({
+        features: () => [
+          BoldFeature(),
+          ItalicFeature(),
+          ParagraphFeature(),
+          HeadingFeature({ enabledHeadingSizes: ['h3', 'h4'] }),
+          UnorderedListFeature(),
+          OrderedListFeature(),
+          LinkFeature(), // TODO: Customize link fields
+          BlockquoteFeature(),
+          InlineToolbarFeature(),
+          FixedToolbarFeature(),
+        ],
+      }),
+    },
+  ],
+};
