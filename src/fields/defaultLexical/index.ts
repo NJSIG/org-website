4263cd513970b941b3e7bd708f8335cffc925548
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-07-03 12:22:02 -0400

    feat(fields): default lexical link field, no url validation for internal links

--- src/fields/defaultLexical/index.ts
+++ src/fields/defaultLexical/index.ts
@@ -7,10 +7,12 @@ import {
   ItalicFeature,
   lexicalEditor,
   LinkFeature,
+  LinkFields,
   OrderedListFeature,
   ParagraphFeature,
   UnorderedListFeature,
 } from '@payloadcms/richtext-lexical';
+import { TextFieldSingleValidation } from 'payload';
 
 export const defaultLexical = lexicalEditor({
   features: [
@@ -24,8 +26,29 @@ export const defaultLexical = lexicalEditor({
     LinkFeature({
       enabledCollections: ['pages', 'events'],
       fields: ({ defaultFields }) => {
-        console.log('defaultFields', defaultFields);
-        return defaultFields;
+        const defaultFieldsWithoutUrl = defaultFields.filter((field) => {
+          if ('name' in field && field.name === 'url') return false;
+          return true;
+        });
+
+        return [
+          ...defaultFieldsWithoutUrl,
+          {
+            name: 'url',
+            type: 'text',
+            admin: {
+              condition: (_data, siblingData) => siblingData?.linkType !== 'internal',
+            },
+            label: ({ t }) => t('fields:enterURL'),
+            required: true,
+            validate: ((value, options) => {
+              if ((options?.siblingData as LinkFields)?.linkType === 'internal') {
+                return true; // No url validation needed for internal links
+              }
+              return value ? true : 'URL is required';
+            }) as TextFieldSingleValidation,
+          },
+        ];
       },
     }),
     InlineToolbarFeature(),
