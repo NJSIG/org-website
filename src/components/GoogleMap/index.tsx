b6f83540fbfc2e1456400e2e436aaeca77de6a2b
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-07-11 11:31:43 -0400

    fix(fields): ui map field now renders from location or entry fields

--- src/components/GoogleMap/index.tsx
+++ src/components/GoogleMap/index.tsx
@@ -45,28 +45,28 @@ export const GoogleMap = (props: MapProps) => {
   const apiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
   const height = heightFromProps && heightFromProps >= 200 ? heightFromProps : 200;
   const width = widthFromProps && widthFromProps >= 200 ? widthFromProps : undefined;
-  const query = encodeURI(
-    Object.entries(location)
-      // Filter out empty values
-      .filter(([, value]) => Boolean(value))
-      // Merge streetAddress and streetAddress2 if both are present
-      .reduce((acc, [key, value]) => {
-        if (key === 'streetAddress' || key === 'streetAddress2') {
-          if (value) {
-            acc.push(value as string);
-          }
-        } else if (key !== 'streetAddress2') {
-          acc.push(value as string);
+  const query = Object.entries(location)
+    // Filter out empty values
+    .filter(([, value]) => Boolean(value))
+    // Merge streetAddress and streetAddress2 if both are present
+    .reduce((acc, [key, value]) => {
+      if (key === 'streetAddress' || key === 'streetAddress2') {
+        if (value) {
+          acc.push(encodeURIComponent(value as string));
         }
-        return acc;
-      }, [] as string[])
-      .join(','),
-  );
-
+      } else if (key !== 'streetAddress2') {
+        acc.push(encodeURIComponent(value as string));
+      }
+      return acc;
+    }, [] as string[])
+    .join(',');
   const src = `https://www.google.com/maps/embed/v1/${mode}?key=${apiKey}&q=${query}`;
 
   return (
-    <div className={cn({ 'njsig__map-container': admin, '': !admin }, containerClassName)}>
+    <div
+      className={cn({ 'njsig__map-container': admin, '': !admin }, containerClassName)}
+      style={admin ? { height: `${height}px`, width: width ? `${width}px` : '100%' } : {}}
+    >
       {apiKey !== '' && location !== null && query !== '' ? (
         <iframe
           width={width || '100%'}
--- src/fields/uiMap/MapComponent.tsx
+++ src/fields/uiMap/MapComponent.tsx
@@ -2,9 +2,11 @@
 
 import { GoogleMap } from '@/components/GoogleMap';
 import { useFormFields } from '@payloadcms/ui';
+import { useEffect, useMemo, useState } from 'react';
 
 type Props = {
   locationField?: string;
+  collectionSlug?: string;
 };
 
 type LocationData = {
@@ -16,7 +18,10 @@ type LocationData = {
   zipCode?: string;
 };
 
-export const MapComponent: React.FC<Props> = ({ locationField = 'location' }) => {
+export const MapComponent: React.FC<Props> = ({
+  locationField = 'location',
+  collectionSlug = 'locations',
+}) => {
   const formData = useFormFields(([fields]) => ({
     // Location Collection Fields
     name: fields.name,
@@ -29,18 +34,80 @@ export const MapComponent: React.FC<Props> = ({ locationField = 'location' }) =>
     relatedLocation: fields[locationField],
   }));
 
-  console.log('Form Data', formData);
+  const [location, setLocation] = useState<LocationData>({
+    name: '',
+    streetAddress: '',
+    streetAddress2: '',
+    city: '',
+    state: '',
+    zipCode: '',
+  });
 
-  const location: LocationData = {
-    name: (formData.name?.value || '') as string,
-    streetAddress: (formData.streetAddress?.value || '') as string,
-    streetAddress2: (formData.streetAddress2?.value || '') as string,
-    city: (formData.city?.value || '') as string,
-    state: (formData.state?.value || '') as string,
-    zipCode: (formData.zipCode?.value || '') as string,
-  };
+  const formDataValues = useMemo(() => {
+    return {
+      name: (formData.name?.value || '') as string,
+      streetAddress: (formData.streetAddress?.value || '') as string,
+      streetAddress2: (formData.streetAddress2?.value || '') as string,
+      city: (formData.city?.value || '') as string,
+      state: (formData.state?.value || '') as string,
+      zipCode: (formData.zipCode?.value || '') as string,
+      relatedLocation: (formData.relatedLocation?.value || '') as string,
+    };
+  }, [
+    formData.name?.value,
+    formData.streetAddress?.value,
+    formData.streetAddress2?.value,
+    formData.city?.value,
+    formData.state?.value,
+    formData.zipCode?.value,
+    formData.relatedLocation?.value,
+  ]);
 
-  console.log('Location', location);
+  useEffect(() => {
+    const baseLocation: LocationData = {
+      name: formDataValues.name,
+      streetAddress: formDataValues.streetAddress,
+      streetAddress2: formDataValues.streetAddress2,
+      city: formDataValues.city,
+      state: formDataValues.state,
+      zipCode: formDataValues.zipCode,
+    };
+
+    // If there is a related location, we can fetch it from the API
+    if (
+      formDataValues.relatedLocation !== undefined &&
+      formDataValues.relatedLocation !== null &&
+      formDataValues.relatedLocation !== ''
+    ) {
+      fetchLocationData(collectionSlug, formDataValues.relatedLocation).then((relatedLocation) => {
+        setLocation(() => ({
+          name: relatedLocation.name || '',
+          streetAddress: relatedLocation.streetAddress || '',
+          streetAddress2: relatedLocation.streetAddress2 || '',
+          city: relatedLocation.city || '',
+          state: relatedLocation.state || '',
+          zipCode: relatedLocation.zipCode || '',
+        }));
+      });
+    } else {
+      setLocation(baseLocation);
+    }
+  }, [formDataValues, collectionSlug]);
 
   return <GoogleMap height={400} location={location} admin={true} />;
 };
+
+async function fetchLocationData(
+  collectionSlug: string,
+  locationID: string,
+): Promise<LocationData> {
+  try {
+    const response = await fetch(`/api/${collectionSlug}/${locationID}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching location data:', error);
+  }
+
+  return {};
+}
