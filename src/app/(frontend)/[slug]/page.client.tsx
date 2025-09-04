ddae66e6c681b1cfe8da989fb5a2d7c4af513b8d
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-09-04 11:07:48 -0400

    fix(frontend): remove subfund class from pages template

--- src/app/(frontend)/[slug]/page.client.tsx
+++ src/app/(frontend)/[slug]/page.client.tsx
@@ -1,36 +1,22 @@
 'use client';
 
-import { SubfundTheme, Templates } from '@/fields/dynamicBlocks/types';
+import { Templates } from '@/fields/dynamicBlocks/types';
 import { useHeaderTheme } from '@/providers/HeaderTheme';
 import { useSubfundTheme } from '@/providers/SubfundTheme';
 import React, { useEffect } from 'react';
 
 type PageClientProps = {
   template?: Templates;
-  subfund?: SubfundTheme | undefined | null;
 };
 
 const PageClient: React.FC<PageClientProps> = (props) => {
-  const { template = 'default', subfund = null } = props;
-
   const { setHeaderTheme } = useHeaderTheme();
   const { setSubfundTheme } = useSubfundTheme();
 
   useEffect(() => {
-    if (template === 'subfund') {
-      setHeaderTheme('light');
-    } else {
-      setHeaderTheme('dark');
-    }
-  }, [template, setHeaderTheme]);
-
-  useEffect(() => {
-    if (template === 'subfund') {
-      setSubfundTheme(subfund);
-    } else {
-      setSubfundTheme(null);
-    }
-  }, [subfund, template, setSubfundTheme]);
+    setHeaderTheme('dark');
+    setSubfundTheme(null);
+  }, [setHeaderTheme, setSubfundTheme]);
 
   return <React.Fragment />;
 };
