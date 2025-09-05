265b46c10d9fbfc203744a19a6b84f78a73e6c8d
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-09-05 14:10:22 -0400

    feat(blocks): made title theme component themeable

--- src/components/TitleTheme/index.tsx
+++ src/components/TitleTheme/index.tsx
@@ -28,7 +28,7 @@ const textVariants = cva('font-bold text-foreground dark:text-foreground-inverte
   },
 });
 
-const underlineVariants = cva('bg-njsig-accent-midtone', {
+const underlineVariants = cva('bg-[var(--title-theme-accent)]', {
   variants: {
     size: {
       small: 'h-1',
