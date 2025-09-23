e33321ae3c5b52159b82f8af5a2a1ce08e21e734
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-09-23 15:34:55 -0400

    chore: set db name in payload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -101,6 +101,9 @@ export default buildConfig({
   },
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
+    connectOptions: {
+      dbName: 'payload',
+    },
   }),
   email: nodemailerAdapter({
     defaultFromAddress: 'noreply@njsig.org',
