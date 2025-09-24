273ad2e3657cf1105b72a8fae7becd21169b7095
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-09-24 14:59:17 -0400

    chore: remove database name param until ready to move to pr

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -101,9 +101,9 @@ export default buildConfig({
   },
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
-    connectOptions: {
-      dbName: 'payload',
-    },
+    // connectOptions: {
+    //   dbName: 'payload',
+    // },
   }),
   email: nodemailerAdapter({
     defaultFromAddress: 'noreply@njsig.org',
