4286a9acd5be9f8faf5d0cf6e5d5d55c74d3a5cc
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-11-21 10:06:49 -0500

    fix(page): sub-fund - now uses theme for map image name

--- src/app/(frontend)/sub-funds/[slug]/page.client.tsx
+++ src/app/(frontend)/sub-funds/[slug]/page.client.tsx
@@ -72,7 +72,7 @@ const SubfundPageClient: React.FC<SubfundPageClientProps> = ({
           </div>
           <Image
             className="hidden md:block absolute right-0 -top-4 w-52 h-auto"
-            src={`/assets/sub-funds/${subfund.slug}-map.svg`}
+            src={`/assets/sub-funds/${subfund.theme}-map.svg`}
             alt={`${subfund.shortName} Map`}
             width={0}
             height={0}
