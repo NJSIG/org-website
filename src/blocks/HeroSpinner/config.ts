5d06dbedb88101a9c4b8a44de7691237a5995cea
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2025-12-11 15:19:49 -0500

    fix: add tip to hero spinner block

--- src/blocks/HeroSpinner/config.ts
+++ src/blocks/HeroSpinner/config.ts
@@ -1,4 +1,5 @@
 import { linkField } from '@/fields/link';
+import { uiTipField } from '@/fields/uiTip';
 import { Block } from 'payload';
 
 export const HeroSpinner: Block = {
@@ -7,6 +8,9 @@ export const HeroSpinner: Block = {
   imageURL: '/blocks/hero-spinner.png',
   imageAltText: 'Hero Spinner Block',
   fields: [
+    uiTipField([
+      'Hero Spinner is a special display heading. A Hidden Title should be used for accessability.',
+    ]),
     {
       name: 'slideTimeout',
       label: 'Slide Timeout (seconds)',
