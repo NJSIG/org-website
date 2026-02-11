8c29250bf6c811e44d18c66821e17c6bf650ea0a
Michael Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-02-11 17:12:02 -0500

    fix(analytics): update captureOnLocalhost schema to handle undefined values

--- src/providers/PlausibleConfigProvider/types.ts
+++ src/providers/PlausibleConfigProvider/types.ts
@@ -3,7 +3,10 @@ import z from 'zod';
 export const PlausibleConfigSchema = z.object({
   domain: z.string(),
   host: z.string(),
-  captureOnLocalhost: z.string().transform((value) => value === 'true'),
+  captureOnLocalhost: z
+    .undefined()
+    .transform(() => false)
+    .or(z.string().transform((val) => val === 'true')),
 });
 
 export type PlausibleConfig = z.infer<typeof PlausibleConfigSchema>;
