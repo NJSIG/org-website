979a594d555276ecb7ec12180e7b1bc60ef8c2da
Michael Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-02-27 15:51:05 -0500

    refactor: updated callout card styles changed callout card to have rounded corners to match the rest of the site design language

--- src/components/CalloutCard/index.tsx
+++ src/components/CalloutCard/index.tsx
@@ -8,16 +8,22 @@ export const CalloutCard: React.FC<{
   return (
     <div
       className={cn(
-        'dark flex flex-col gap-6 p-8 bg-njsig-shade text-foreground-inverted',
-        {
-          'mb-2 xl:mb-2.5': shadow,
-          'shadow-(--callout-shadow-sm-l) xl:shadow-(--callout-shadow-md-l)': shadow === 'left',
-          'shadow-(--callout-shadow-sm-r) xl:shadow-(--callout-shadow-md-r)': shadow === 'right',
-        },
+        'dark flex flex-col gap-6 p-8 bg-njsig-shade text-foreground-inverted rounded-lg relative',
         className,
       )}
     >
       {children}
+      {shadow && (
+        <div
+          className={cn(
+            'absolute h-full w-full -z-1 bg-njsig-accent-midtone rounded-lg -bottom-2 xl:-bottom-2.5',
+            {
+              '-left-2 xl:-left-2.5 rounded-bl-2xl xl:rounded-bl-[18px]': shadow === 'left',
+              '-right-2 xl:-right-2.5 rounded-br-2xl xl:rounded-br-[18px]': shadow === 'right',
+            },
+          )}
+        />
+      )}
     </div>
   );
 };
