455a304fe73caddf62036dbe5cbef466c17afd6f
Michael Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-03-04 15:29:16 -0500

    fix(block): fix bad width on section content, add gutter and centering options

--- src/blocks/Section/blocks/SectionContent/config.ts
+++ src/blocks/Section/blocks/SectionContent/config.ts
@@ -7,6 +7,28 @@ export const SectionContent: Block = {
   imageAltText: 'Section Content Block',
   fields: [
     {
+      type: 'row',
+      fields: [
+        {
+          name: 'centerBlock',
+          type: 'checkbox',
+          defaultValue: false,
+          admin: {
+            description: 'If enabled, the content will be centered within the section.',
+          },
+        },
+        {
+          name: 'enableGutter',
+          type: 'checkbox',
+          defaultValue: false,
+          admin: {
+            description:
+              'Add a gutter for horizontal padding when using outside of a section block.',
+          },
+        },
+      ],
+    },
+    {
       name: 'content',
       type: 'richText',
       required: true,
