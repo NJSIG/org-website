c524df8bb3aa7b9b0c51e8a3018aa9386d7a7fb7
Michael Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-04-02 16:08:44 -0400

    feat: related cards component (wip)

--- src/blocks/RelatedCards/Component.tsx
+++ src/blocks/RelatedCards/Component.tsx
@@ -1,5 +1,23 @@
+import { Hyperlink } from '@/components/Hyperlink';
 import { RelatedCardsBlock as RelatedCardsBlockProps } from '@/payload-types';
+import { ArrowUpRightIcon } from 'lucide-react';
 
 export const RelatedCardsBlock: React.FC<RelatedCardsBlockProps> = ({ cards }) => {
-  return <div>Related Cards works!</div>;
+  return (
+    <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 items-stretch">
+      {cards?.map(({ id, title, description, link }, index) => (
+        <Hyperlink
+          key={id ?? `related-card-${index}`}
+          link={link}
+          className="flex flex-col lg:grow lg:shrink lg:basis-0 gap-4 hover:motion-safe:[&_svg]:animate-micro-up-right"
+        >
+          <h5 className="flex justify-between">
+            <span>{title}</span>
+            <ArrowUpRightIcon size={24}></ArrowUpRightIcon>
+          </h5>
+          <p>{description}</p>
+        </Hyperlink>
+      ))}
+    </div>
+  );
 };
