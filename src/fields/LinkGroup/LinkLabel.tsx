be486bed214dee44385e458b4b013cec4c1ca1c1
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-04-15 14:11:14 -0400

    chore: move link label generation to util

--- src/fields/LinkGroup/LinkLabel.tsx
+++ src/fields/LinkGroup/LinkLabel.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { Event, Page, Subfund } from '@/payload-types';
-import { isObject } from '@/utilities/isObject';
+import { getLinkReferenceLabel } from '@/utilities/getLinkReferenceLable';
 import { RowLabelProps, useRowLabel } from '@payloadcms/ui';
 import { Binoculars, ExternalLink, Globe, Link } from 'lucide-react';
 import React from 'react';
@@ -11,31 +10,13 @@ interface LinkLabel {
   link: LinkField;
 }
 
-const getReferenceLabel = (link: LinkField): string => {
-  if (isObject(link.reference?.value)) {
-    const value: unknown = link.reference.value;
-    switch (link.reference.relationTo) {
-      case 'pages':
-        return (value as Page).title;
-      case 'events':
-        return (value as Event).title;
-      case 'subfunds':
-        return (value as Subfund).shortName;
-      default:
-        return 'Unknown Reference';
-    }
-  }
-
-  return link.reference?.value ?? 'Unknown Reference';
-};
-
 const LinkLabel: React.FC<RowLabelProps> = () => {
   const { rowNumber, data } = useRowLabel<LinkLabel>();
   const link = data?.link;
   const label =
     (link?.label && link?.label !== '' ? link.label : undefined) ??
     (link?.type === 'reference' && link?.reference
-      ? `Reference to: ${link.reference?.relationTo} - ${getReferenceLabel(link)}`
+      ? `Reference to: ${link.reference?.relationTo} - ${getLinkReferenceLabel(link)}`
       : undefined) ??
     (link?.type === 'custom' && link?.url ? `Custom URL: ${link.url}` : undefined) ??
     `Link ${rowNumber !== undefined ? rowNumber + 1 : ''}`;
