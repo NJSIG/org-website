4c3c0bbb99d4c752498f56dce8df7bab4ad10456
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-05-29 09:35:14 -0400

    fix(page): event year page now checks if the provided year is 4 digits. This fixes an issue where a link from old website could result is show events for a nonsensical year like `413`

--- src/app/(frontend)/events/[year]/page.tsx
+++ src/app/(frontend)/events/[year]/page.tsx
@@ -6,6 +6,13 @@ type Args = {
 
 export default async function EventsYearPage({ params: paramsPromise }: Args) {
   const { year } = await paramsPromise;
+
+  // The old website linked to events by ID, we know the old IDs were only 3 digits,
+  // so we can just test if the year is 4 digits and redirect if it isn't.
+  if (!/^\d{4}$/.test(year)) {
+    redirect('/events');
+  }
+
   const date = new Date();
 
   redirect(`/events/${year}/${(date.getMonth() + 1).toString().padStart(2, '0')}`);
