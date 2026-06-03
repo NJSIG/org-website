ed0204c8151b33d5ceb9a1f7dd9114c31fd9c854
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-06-03 08:47:24 -0400

    chore(refactor): move section block types definition to the section root

--- src/blocks/Section/types.ts
+++ src/blocks/Section/types.ts
@@ -1,5 +1,6 @@
 import {
   CMSButtonBlock,
+  ContactsListBlock,
   EmphasizedListBlock,
   EventTilesBlock,
   IconListBlock,
@@ -20,6 +21,7 @@ export type SectionWidthOptions = Record<SectionWidths, Options>;
 
 type SectionBlocks =
   | CMSButtonBlock
+  | ContactsListBlock
   | EmphasizedListBlock
   | EventTilesBlock
   | IconListBlock
