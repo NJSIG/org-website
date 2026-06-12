aae8594c221adc284c91cd0e7475c80c9998f58c
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-06-12 10:19:54 -0400

    fix(util): generate event link now returns a `404` link if no slug is provided.

--- src/utilities/generateEventLink.ts
+++ src/utilities/generateEventLink.ts
@@ -1,10 +1,13 @@
-export const generateEventLink = (event: {
-  slug?: string | null;
-  startDate: string;
-  [k: string]: unknown;
-}): string => {
-  const { slug, startDate } = event;
-  const date = new Date(startDate);
+import { Event } from '@/payload-types';
+import { Temporal } from '@js-temporal/polyfill';
 
-  return `/events/${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}/${slug}`;
+export const generateEventLink = (event: Pick<Event, 'slug' | 'startDate'>): string => {
+  const { slug, startDate } = event;
+  const date = Temporal.PlainDate.from(startDate.slice(0, 10)); // Extract date portion and convert to Temporal.PlainDate
+
+  if (!slug) {
+    return '/404'; // Fallback URL if slug is missing
+  }
+
+  return `/events/${date.year}/${date.month.toString().padStart(2, '0')}/${date.day.toString().padStart(2, '0')}/${slug}`;
 };
