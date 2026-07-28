3e38a2937a06b0719d758a599218cfa4f6b9415e
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-07-28 10:31:57 -0400

    feat(collection): add presentation title to events

--- src/collections/Events/index.ts
+++ src/collections/Events/index.ts
@@ -25,6 +25,7 @@ export const Events: CollectionConfig<'events'> = {
   // Type safe if the collection slug generic is passed to `CollectionConfig` - `CollectionConfig<'pages'>
   defaultPopulate: {
     title: true,
+    eventType: true,
     slug: true,
     startDate: true,
     startTime: true,
@@ -49,6 +50,18 @@ export const Events: CollectionConfig<'events'> = {
       localized: true,
       admin: {
         description: 'The title of the page, used for routing, SEO, tabs, and the admin UI.',
+      },
+    },
+    {
+      name: 'presentationTitle',
+      type: 'text',
+      localized: true,
+      admin: {
+        description:
+          'The title of the presentation, falls back to the event title if not provided.',
+        condition: (_, siblingData) =>
+          siblingData.eventType !== EventTypes.TrusteeMeeting.value &&
+          siblingData.eventType !== EventTypes.ImportantDate.value,
       },
     },
     {
