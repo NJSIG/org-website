2adc5c2884af1e52d202a94a73f488a6518e0055
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-08-12 15:30:57 -0400

    wip: new sub-fund capsule

--- src/components/SubfundCapsule/index.tsx
+++ src/components/SubfundCapsule/index.tsx
@@ -3,8 +3,6 @@ import coolifyImageLoader from '@/utilities/coolifyImageLoader';
 import { ArrowUpRightIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ContactPerson } from '../ContactPerson';
-import { RichText } from '../RichText';
 
 type SubfundCapsuleProps = {
   subfund: Subfund;
@@ -15,42 +13,13 @@ export const SubfundCapsule: React.FC<SubfundCapsuleProps> = ({ subfund }) => {
     <Link
       href={`/sub-funds/${subfund.slug}`}
       data-subfund-theme={subfund.theme}
-      className="subfund-theme flex flex-col gap-4 p-6 rounded-3xl bg-(--subfund-capsule-bg) group/capsule w-full max-w-section hover:bg-mix-shade-(--subfund-capsule-bg)/2 transition-colors relative min-h-72"
+      className="subfund-theme group/capsule relative flex w-full items-center rounded-3xl bg-(--subfund-capsule-bg) p-6 transition-colors hover:bg-mix-shade-(--subfund-capsule-bg)/2"
     >
-      <div className="flex items-center justify-between w-full pb-2 border-b-[6px] border-(--subfund-accent)">
-        <h3 className="text-5xl font-extrabold text-(--subfund-foreground)">{subfund.shortName}</h3>
-        <ArrowUpRightIcon
-          size="40"
-          className="group-hover/capsule:motion-safe:animate-micro-up-right stroke-(--subfund-foreground)"
-        />
-      </div>
-      <RichText data={subfund.content.summary} className="prose-base mx-0 max-w-section-content" />
-      <div className="flex flex-col gap-4 md:flex-row">
-        {subfund.content.administrators && (
-          <div className="flex flex-col gap-2">
-            {subfund.content.administrators.map((admin) => {
-              if (typeof admin === 'string') {
-                return null;
-              }
-
-              return <ContactPerson key={admin.id} contact={admin} size="sm" />;
-            })}
-          </div>
-        )}
-        {subfund.content.reps && (
-          <div className="flex flex-col gap-2">
-            {subfund.content.reps.map((rep) => {
-              if (typeof rep === 'string') {
-                return null;
-              }
-
-              return <ContactPerson key={rep.id} contact={rep} size="sm" />;
-            })}
-          </div>
-        )}
-      </div>
+      <h3 className="grow text-5xl font-extrabold text-(--subfund-foreground) after:absolute after:block after:h-2.5 after:w-full after:bg-(--subfund-accent)">
+        {subfund.shortName}
+      </h3>
       <Image
-        className="hidden md:block absolute right-16 top-8 w-40 h-auto"
+        className="h-32 w-auto"
         src={`/assets/sub-funds/${subfund.theme}-map.svg`}
         alt={`${subfund.shortName} Map`}
         height={0}
@@ -58,6 +27,37 @@ export const SubfundCapsule: React.FC<SubfundCapsuleProps> = ({ subfund }) => {
         unoptimized
         loader={coolifyImageLoader}
       />
+      <ArrowUpRightIcon
+        size="24"
+        className="self-start stroke-(--subfund-foreground) group-hover/capsule:motion-safe:animate-micro-up-right"
+      />
     </Link>
+
+    // <Link
+    //   href={`/sub-funds/${subfund.slug}`}
+    //   data-subfund-theme={subfund.theme}
+    //   className="subfund-theme group/capsule flex w-full flex-col rounded-3xl bg-(--subfund-capsule-bg) p-6 transition-colors hover:bg-mix-shade-(--subfund-capsule-bg)/2"
+    // >
+    //   <ArrowUpRightIcon
+    //     size="24"
+    //     className="ml-auto stroke-(--subfund-foreground) group-hover/capsule:motion-safe:animate-micro-up-right"
+    //   />
+    //   <div className="relative flex min-h-44 items-center">
+    //     <div className="w-full border-b-10 border-(--subfund-accent) pb-2">
+    //       <h3 className="text-5xl font-extrabold text-(--subfund-foreground)">
+    //         {subfund.shortName}
+    //       </h3>
+    //     </div>
+    //   </div>
+    //   {/* <Image
+    //     className="absolute top-8 right-16 hidden h-32 w-auto md:block"
+    //     src={`/assets/sub-funds/${subfund.theme}-map.svg`}
+    //     alt={`${subfund.shortName} Map`}
+    //     height={0}
+    //     width={0}
+    //     unoptimized
+    //     loader={coolifyImageLoader}
+    //   /> */}
+    // </Link>
   );
 };
