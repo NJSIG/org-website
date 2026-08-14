47f269dc85dd1cfcef264914f8292f02b05a06a8
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-08-14 14:21:53 -0400

    chore: add debug logging to get client side url util

--- src/utilities/getClientSideUrl.ts
+++ src/utilities/getClientSideUrl.ts
@@ -6,12 +6,19 @@ export const getClientSideUrl = () => {
     const domain = window.location.hostname;
     const port = window.location.port;
 
+    console.log('Client Side URL - Can use DOM:', { protocol, domain, port });
+
     return `${protocol}//${domain}${port ? `:${port}` : ''}`;
   }
 
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
+
+  console.log(
+    'Client Side URL - Fallback to NEXT_PUBLIC_SERVER_URL:',
+    process.env.NEXT_PUBLIC_SERVER_URL || '',
+  );
 
   return process.env.NEXT_PUBLIC_SERVER_URL || '';
 };
