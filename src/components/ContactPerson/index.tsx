f624ed989bffbeb9a5e3f00ce2094119f0008cd9
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2026-08-14 10:46:09 -0400

    refactor(component): improve portrait component selection based on rendering options

--- src/components/ContactPerson/index.tsx
+++ src/components/ContactPerson/index.tsx
@@ -19,11 +19,19 @@ type ContactPersonProps = {
   className?: string;
 };
 
+type PortraitProps = {
+  name: Contact['name'];
+  portrait: Contact['portrait'];
+  size: 'sm' | 'md';
+  type: Contact['type'];
+  priority?: boolean;
+};
+
 const portraitVariants = cva(['rounded-full'], {
   variants: {
     size: {
-      sm: 'border-2 size-10',
-      md: 'border-2 size-14',
+      sm: 'size-10 border-2',
+      md: 'size-14 border-2',
     },
     type: {
       njsig: 'border-njsig-primary',
@@ -72,47 +80,94 @@ export const ContactPerson: React.FC<ContactPersonProps> = ({
         className,
       )}
     >
-      {showPortrait !== ContactPersonPortraitOptions.Never &&
-      portrait &&
-      typeof portrait === 'object' ? (
-        <Image
-          alt={`${name} Portrait`}
-          width={size === 'sm' ? 40 : 56}
-          height={size === 'sm' ? 40 : 56}
-          priority={priority}
-          quality={90}
-          decoding="async"
-          src={
-            (portrait as ContactPortrait).url
-              ? (portrait as ContactPortrait).url!
-              : `/assets/placeholders/contact-${size}.webp`
-          }
-          placeholder="blur"
-          blurDataURL={blurDataToBlurDataURL((portrait as ContactPortrait)?.blurData)}
-          className={portraitVariants({ size, type })}
-          loader={coolifyImageLoader}
-        />
-      ) : showPortrait === ContactPersonPortraitOptions.Always ? (
-        <Image
-          alt={`${name} Placeholder`}
-          width={size === 'sm' ? 40 : 56}
-          height={size === 'sm' ? 40 : 56}
-          priority={priority}
-          decoding="async"
-          quality={90}
-          src={`/assets/placeholders/contact-${size}.webp`}
-          className={portraitVariants({ size, type })}
-          unoptimized
-          loader={coolifyImageLoader}
-        />
-      ) : null}
+      {(() => {
+        switch (showPortrait) {
+          case ContactPersonPortraitOptions.Always:
+            return portrait && typeof portrait === 'object' ? (
+              <ContactPortraitImage
+                name={name}
+                portrait={portrait}
+                size={size}
+                type={type}
+                priority={priority}
+              />
+            ) : (
+              <ContactPlaceholderImage name={name} size={size} type={type} priority={priority} />
+            );
+          case ContactPersonPortraitOptions.IfAvailable:
+            return portrait && typeof portrait === 'object' ? (
+              <ContactPortraitImage
+                name={name}
+                portrait={portrait}
+                size={size}
+                type={type}
+                priority={priority}
+              />
+            ) : null;
+          case ContactPersonPortraitOptions.Never:
+            return null;
+          default:
+            return null;
+        }
+      })()}
       <div className={cn('flex flex-col', { 'gap-0': size === 'sm', 'gap-1': size === 'md' })}>
         <span className={cn('font-bold', { 'text-sm': size === 'sm', 'text-base': size === 'md' })}>
           {name}
         </span>
-        {resolvedTitle && <small className="font-medium text-xs">{resolvedTitle}</small>}
+        {resolvedTitle && <small className="text-xs font-medium">{resolvedTitle}</small>}
         {type !== 'njsig' && organization && <small className="text-xs">{organization}</small>}
       </div>
     </div>
+  );
+};
+
+const ContactPortraitImage: React.FC<PortraitProps> = ({
+  name,
+  portrait,
+  size,
+  type,
+  priority,
+}) => {
+  console.log('Rendering ContactPortrait:', portrait);
+  return (
+    <Image
+      alt={`${name} Portrait`}
+      width={size === 'sm' ? 40 : 56}
+      height={size === 'sm' ? 40 : 56}
+      priority={priority}
+      quality={90}
+      decoding="async"
+      src={
+        (portrait as ContactPortrait).url
+          ? (portrait as ContactPortrait).url!
+          : `/assets/placeholders/contact-${size}.webp`
+      }
+      placeholder="blur"
+      blurDataURL={blurDataToBlurDataURL((portrait as ContactPortrait)?.blurData)}
+      className={portraitVariants({ size, type })}
+      loader={coolifyImageLoader}
+    />
+  );
+};
+
+const ContactPlaceholderImage: React.FC<Omit<PortraitProps, 'portrait'>> = ({
+  name,
+  size,
+  type,
+  priority,
+}) => {
+  return (
+    <Image
+      alt={`${name} Placeholder`}
+      width={size === 'sm' ? 40 : 56}
+      height={size === 'sm' ? 40 : 56}
+      priority={priority}
+      decoding="async"
+      quality={90}
+      src={`/assets/placeholders/contact-${size}.webp`}
+      className={portraitVariants({ size, type })}
+      unoptimized
+      loader={coolifyImageLoader}
+    />
   );
 };
